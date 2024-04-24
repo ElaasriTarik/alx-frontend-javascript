cdd0499e93a97afa089ddb14073302f47c93b1cf
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-04-24 12:04:29 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,17 @@ import signup from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(fursstname, lastname, filename) {
-  return Promise.all([uploadPhoto(filename), signup(fursstname, lastname)])
-    .then((res) => [{
-      status: res.status,
-      value: res.value,
-    },
-    {
-      status: res.status,
-      value: res.value,
-    }]).then((error) => error);
+  return Promise.allSettled([uploadPhoto(filename), signup(fursstname, lastname)])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: 'fulfilled',
+          value: result.value,
+        };
+      }
+      return {
+        status: 'rejected',
+        reason: result.reason,
+      };
+    }));
 }
