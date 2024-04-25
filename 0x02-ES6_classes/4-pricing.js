009a6c6621fa0d8536cc191c41840dc3cdbcab10
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-04-25 17:47:43 +0100

    classes task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,34 +1,34 @@
 import Currency from './3-currency';
 
 export default class Pricing extends Currency {
-	constructor(amount, currency) {
-		super(currency.code, currency.name);
-		this._amount = amount;
-		this._currency = currency;
-	}
+  constructor(amount, currency) {
+    super(currency.code, currency.name);
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-	// getters and setters
-	get amount() {
-		return this._amount;
-	}
+  // getters and setters
+  get amount() {
+    return this._amount;
+  }
 
-	set amount(amount) {
-		this._amount = amount;
-	}
+  set amount(amount) {
+    this._amount = amount;
+  }
 
-	get currency() {
-		return this._currency;
-	}
+  get currency() {
+    return this._currency;
+  }
 
-	set currency(currency) {
-		this._currency = currency;
-	}
+  set currency(currency) {
+    this._currency = currency;
+  }
 
-	displayFullPrice() {
-		return `${this._amount} ${this._currency.displayFullCurrency()}`;
-	}
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
 
-	static convertPrice(amount, conversionRate) {
-		return amount * conversionRate;
-	}
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
