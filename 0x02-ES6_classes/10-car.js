42d9aac18bc3475c913b772e7c3829f4d0596255
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-04-25 22:58:23 +0100

    classes task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,12 +1,12 @@
 export default class Car {
-  constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
-  }
+	constructor(brand, motor, color) {
+		this._brand = brand;
+		this._motor = motor;
+		this._color = color;
+	}
 
-  cloneCar() {
-    return this.constructor[Symbol.species];
-    // return new carClone();
-  }
+	cloneCar() {
+		return this.constructor[Symbol.species];
+		//return new CarClone();
+	}
 }
