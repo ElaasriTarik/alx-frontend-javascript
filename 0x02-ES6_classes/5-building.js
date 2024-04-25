29c1b4b9273469ce8aa3223e07050cd481e28a29
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-04-25 21:48:10 +0100

    classes task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,12 +1,12 @@
 export default class Building {
-  constructor(sqft) {
-    if (this.constructor !== Building) {
-      throw new Error('Building class is abstract and cannot be instantiated directly!');
-    }
-    this._sqft = sqft;
-  }
+	constructor(sqft) {
+		if (this.constructor !== Building) {
+			throw new Error('Class extending Building must override evacuationWarningMessage');
+		}
+		this._sqft = sqft;
+	}
 
-  get sqft() {
-    return this._sqft;
-  }
+	get sqft() {
+		return this._sqft;
+	}
 }
